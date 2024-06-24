42b2006c3cf359517e5d1d8b9ebe323fa9fda706
Luyao Deng <luyao.deng@student.kit.edu>
2024-06-24 23:34:05 +0200

    remove w2

--- toy_task/GMM/algorithms/algorithm.py
+++ toy_task/GMM/algorithms/algorithm.py
@@ -11,10 +11,9 @@ from toy_task.GMM.targets.abstract_target import AbstractTarget
 from toy_task.GMM.models.model_factory import get_model
 from toy_task.GMM.targets.target_factory import get_target
 from toy_task.GMM.algorithms.visualization.GMM_plot import plot2d_matplotlib
-from toy_task.GMM.algorithms.evaluation.JensenShannon_Div import js_divergence, js_divergence_gate
+from toy_task.GMM.algorithms.evaluation.JensenShannon_Div import js_divergence, js_divergence_gate, kl_divergence_gate
 from toy_task.GMM.algorithms.evaluation.Jeffreys_Div import jeffreys_divergence
 from toy_task.GMM.projections.split_kl_projection import split_kl_projection
-from toy_task.GMM.projections.split_w2_projection import split_w2_projection
 
 
 def train_model(model: ConditionalGMM or ConditionalGMM2 or ConditionalGMM3,
@@ -30,8 +29,7 @@ def train_model(model: ConditionalGMM or ConditionalGMM2 or ConditionalGMM3,
                 project,
                 eps_mean: float or None,
                 eps_cov: float or None,
-                alpha: int or None,
-                proj_type="kl"):
+                alpha: int or None):
 
     # optimizer = optim.Adam(model.parameters(), lr=init_lr, weight_decay=1e-5)
     optimizer = optim.Adam([
@@ -56,7 +54,7 @@ def train_model(model: ConditionalGMM or ConditionalGMM2 or ConditionalGMM3,
         if epoch == 0:
             model.eval()
             with ch.no_grad():
-                plot(model, target, proj_type=proj_type, contexts=plot_contexts)
+                plot(model, target, contexts=plot_contexts)
                 model.to(device)
             model.train()
 
@@ -75,33 +73,19 @@ def train_model(model: ConditionalGMM or ConditionalGMM2 or ConditionalGMM3,
 
                 # prediction step
                 gate_pred, mean_pred, chol_pred = model(b_contexts)
-                if proj_type=="w2":
-                    b_sqrt_old = b_chol_old @ b_chol_old.transpose(-1, -2)
-                    sqrt_pred = chol_pred @ chol_pred.transpose(-1, -2)
-                    cov_pred = sqrt_pred @ sqrt_pred
-                    mean_proj = []
-                    cov_proj = []
-                    for j in range(n_components):
-                        mean_proj_j, sqrt_proj_j = split_w2_projection(mean_pred[:, j], sqrt_pred[:, j],
-                                                                       b_mean_old[:, j].clone().detach(),
-                                                                       b_sqrt_old[:, j].clone().detach(),
-                                                                       eps_mean, eps_cov)
-                        mean_proj.append(mean_proj_j)
-                        cov_proj.append(sqrt_proj_j @ sqrt_proj_j)
-                    mean_proj = ch.stack(mean_proj, dim=1)
-                    cov_proj = ch.stack(cov_proj, dim=1)
-                else:
-                    mean_proj = []
-                    chol_proj = []
-                    for j in range(n_components):
-                        mean_proj_j, chol_proj_j = split_kl_projection(mean_pred[:, j] , chol_pred[:, j],
-                                                                       b_mean_old[:, j].clone().detach(),
-                                                                       b_chol_old[:, j].clone().detach(),
-                                                                       eps_mean, eps_cov)
-                        mean_proj.append(mean_proj_j)
-                        chol_proj.append(chol_proj_j)
-                    mean_proj = ch.stack(mean_proj, dim=1)
-                    chol_proj = ch.stack(chol_proj, dim=1)
+
+                # projection step
+                mean_proj = []
+                chol_proj = []
+                for j in range(n_components):
+                    mean_proj_j, chol_proj_j = split_kl_projection(mean_pred[:, j] , chol_pred[:, j],
+                                                                   b_mean_old[:, j].clone().detach(),
+                                                                   b_chol_old[:, j].clone().detach(),
+                                                                   eps_mean, eps_cov)
+                    mean_proj.append(mean_proj_j)
+                    chol_proj.append(chol_proj_j)
+                mean_proj = ch.stack(mean_proj, dim=1)
+                chol_proj = ch.stack(chol_proj, dim=1)
 
                 # component-wise calculation
                 loss_component = []
@@ -109,52 +93,25 @@ def train_model(model: ConditionalGMM or ConditionalGMM2 or ConditionalGMM3,
                 for j in range(n_components):
                     mean_pred_j = mean_pred[:, j]  # (batched_c, 2)
                     chol_pred_j = chol_pred[:, j]
+                    mean_proj_j = mean_proj[:, j]
+                    chol_proj_j = chol_proj[:, j]
 
-                    if proj_type == "kl":
-                        mean_proj_j = mean_proj[:, j]
-                        chol_proj_j = chol_proj[:, j]
+                    # target and target log probability
+                    model_samples = model.get_rsamples(mean_proj_j, chol_proj_j, n_samples)
+                    log_model_j = model.log_prob(mean_proj_j, chol_proj_j, model_samples)
+                    log_target_j = target.log_prob_tgt(b_contexts, model_samples)
 
-                        # target and target log probability
-                        model_samples = model.get_rsamples(mean_proj_j, chol_proj_j, n_samples)
-                        log_model_j = model.log_prob(mean_proj_j, chol_proj_j, model_samples)
-                        log_target_j = target.log_prob_tgt(b_contexts, model_samples)
+                    # regression step
+                    pred_dist = MultivariateNormal(mean_pred_j, scale_tril=chol_pred_j)
+                    proj_dist = MultivariateNormal(mean_proj_j.clone().detach(), scale_tril=chol_proj_j.clone().detach())
+                    reg_loss = kl_divergence(proj_dist, pred_dist).unsqueeze(1).expand(-1, model_samples.shape[1])
 
-                        # regression step
-                        pred_dist = MultivariateNormal(mean_pred_j, scale_tril=chol_pred_j)
-                        proj_dist = MultivariateNormal(mean_proj_j.clone().detach(), scale_tril=chol_proj_j.clone().detach())
-                        reg_loss = kl_divergence(proj_dist, pred_dist).unsqueeze(1).expand(-1, model_samples.shape[1])
+                    aux_loss = model.auxiliary_reward(j, gate_pred.clone().detach(), mean_proj.clone().detach(),
+                                                      chol_proj.clone().detach(), model_samples)
 
-                        aux_loss = model.auxiliary_reward(j, gate_pred.clone().detach(), mean_proj.clone().detach(),
-                                                          chol_proj.clone().detach(), model_samples)
-
-                        gate_pred_j = gate_pred[:, j].unsqueeze(1).expand(-1, model_samples.shape[1])
-                        approx_reward_j = log_model_j - log_target_j - aux_loss + alpha * reg_loss + gate_pred_j
-                        loss_j = ch.exp(gate_pred_j) * approx_reward_j
-
-                    elif proj_type == "w2":
-                        mean_proj_j = mean_proj[:, j]
-                        cov_pred_j = cov_pred[:, j]
-                        cov_proj_j = cov_proj[:, j]
-
-                        # target and target log probability
-                        model_samples = model.get_rsamples_w2(mean_proj_j, cov_proj_j, n_samples)
-                        log_model_j = model.log_prob_w2(mean_proj_j, cov_proj_j, model_samples)
-                        log_target_j = target.log_prob_tgt(b_contexts, model_samples)
-
-                        # regression step
-                        pred_dist = MultivariateNormal(mean_pred_j, cov_pred_j)
-                        proj_dist = MultivariateNormal(mean_proj_j.clone().detach(), cov_proj_j.clone().detach())
-                        reg_loss = kl_divergence(proj_dist, pred_dist).unsqueeze(1).expand(-1, model_samples.shape[1])
-
-                        aux_loss = model.auxiliary_reward_w2(j, gate_pred.clone().detach(), mean_proj.clone().detach(),
-                                                          cov_proj.clone().detach(), model_samples)
-
-                        gate_pred_j = gate_pred[:, j].unsqueeze(1).expand(-1, model_samples.shape[1])
-                        approx_reward_j = log_model_j - log_target_j - aux_loss + alpha * reg_loss + gate_pred_j
-                        loss_j = ch.exp(gate_pred_j) * approx_reward_j
-
-                    else:
-                        raise ValueError("Invalid projection type")
+                    gate_pred_j = gate_pred[:, j].unsqueeze(1).expand(-1, model_samples.shape[1])
+                    approx_reward_j = log_model_j - log_target_j - aux_loss + alpha * reg_loss + gate_pred_j
+                    loss_j = ch.exp(gate_pred_j) * approx_reward_j
 
                     loss_component.append(loss_j)
                     approx_reward_component.append(approx_reward_j)
@@ -171,6 +128,7 @@ def train_model(model: ConditionalGMM or ConditionalGMM2 or ConditionalGMM3,
                 wandb.log({"train_loss": loss.item()})
         else:
             # with log responsibility but without projection
+            batched_approx_reward = []
             for batch_idx in range(0, train_size, batch_size):
                 # get old distribution for current batch
                 b_contexts = shuffled_contexts[batch_idx:batch_idx + batch_size]
@@ -180,7 +138,7 @@ def train_model(model: ConditionalGMM or ConditionalGMM2 or ConditionalGMM3,
 
                 # component-wise calculation
                 loss_component = []
-                # approx_reward_component = []
+                approx_reward_component = []
                 for j in range(n_components):
                     mean_pred_j = mean_pred[:, j]
                     chol_pred_j = chol_pred[:, j]
@@ -196,9 +154,9 @@ def train_model(model: ConditionalGMM or ConditionalGMM2 or ConditionalGMM3,
                     loss_j = ch.exp(gate_pred_j) * approx_reward_j
 
                     loss_component.append(loss_j)
-                    # approx_reward_component.append(approx_reward_j)
+                    approx_reward_component.append(approx_reward_j)
 
-                # batched_approx_reward.append(ch.stack(approx_reward_component))
+                batched_approx_reward.append(ch.stack(approx_reward_component))
                 loss = ch.sum(ch.stack(loss_component))
                 optimizer.zero_grad()
                 loss.backward()
@@ -214,10 +172,11 @@ def train_model(model: ConditionalGMM or ConditionalGMM2 or ConditionalGMM3,
             with ch.no_grad():
                 approx_reward = ch.cat(batched_approx_reward, dim=1)  # [n_components, n_contexts, n_samples]
                 js_divergence_gates = js_divergence_gate(approx_reward, model, shuffled_contexts, device)
-                js_div = js_divergence(model, target, eval_contexts, device, proj_type)
-                j_div = jeffreys_divergence(model, target, eval_contexts, device, proj_type)
+                kl_gate = kl_divergence_gate(approx_reward, model, shuffled_contexts, device)
+                js_div = js_divergence(model, target, eval_contexts, device)
+                j_div = jeffreys_divergence(model, target, eval_contexts, device)
 
-                plot(model, target, proj_type=proj_type, contexts=plot_contexts)
+                plot(model, target, contexts=plot_contexts)
                 model.to(device)
 
                 # trick from VIPS++ paper
@@ -231,7 +190,8 @@ def train_model(model: ConditionalGMM or ConditionalGMM2 or ConditionalGMM3,
                         eps_cov *= 1.1
                     prev_loss = current_loss
             model.train()
-            wandb.log({"Jensen Shannon Divergence between gates": js_divergence_gates.item(),
+            wandb.log({"reverse KL between gates": kl_gate.item(),
+                       "Jensen Shannon Divergence between gates": js_divergence_gates.item(),
                        "Jensen Shannon Divergence": js_div.item(),
                        "Jeffreys Divergence": j_div.item()})
 
@@ -240,14 +200,13 @@ def train_model(model: ConditionalGMM or ConditionalGMM2 or ConditionalGMM3,
 
 def plot(model: ConditionalGMM,
          target: AbstractTarget,
-         proj_type,
          contexts=None):
     if contexts is None:
         contexts = target.get_contexts(3).to('cpu')
     else:
         contexts = contexts.clone().to('cpu')
 
-    plot2d_matplotlib(target, model.to('cpu'), contexts, min_x=-6.5, max_x=6.5, min_y=-6.5, max_y=6.5, proj_type=proj_type)
+    plot2d_matplotlib(target, model.to('cpu'), contexts, min_x=-6.5, max_x=6.5, min_y=-6.5, max_y=6.5)
     # plot2d_matplotlib(target, model.to('cpu'), contexts, min_x=-10, max_x=10, min_y=-10, max_y=10)
 
 
@@ -296,7 +255,7 @@ def toy_task(config):
                 n_epochs, batch_size, n_context, n_components, n_samples, gate_lr, gaussian_lr, device,
                 project, eps_mean, eps_cov, alpha)
 
-#
+
 # if __name__ == "__main__":
 #     # test
 #     config = {
@@ -307,14 +266,14 @@ def toy_task(config):
 #         "num_gate_layer": 5,
 #         "num_component_layer": 7,
 #         "n_samples": 10,
-#         "gate_lr": 0.0001,
-#         "gaussian_lr": 0.0001,
+#         "gate_lr": 0.00001,
+#         "gaussian_lr": 0.00001,
 #         "model_name": "toy_task_model_2",
 #         "target_name": "funnel",
 #         "target_components": 10,
 #         "dim": 10,
 #         "initialization_type": "xavier",
-#         "project": True,
+#         "project": False,
 #         "eps_mean": 1e-6,
 #         "eps_cov": 1e-6,
 #         "alpha": 50
