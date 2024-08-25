71d08de3b4e0753c77c6e5f904fcafa4d7e646ef
Luyao Deng <luyao.deng@student.kit.edu>
2024-08-25 22:16:55 +0200

    new config

--- main_gmm.py
+++ main_gmm.py
@@ -1,14 +1,14 @@
 from omegaconf import DictConfig, OmegaConf
 import hydra
 import wandb
-from toy_task.GMM.algorithms.algorithm_model2 import toy_task
+from toy_task.GMM.algorithms.algorithm import toy_task
 # from toy_task.GMM.algorithms.algorithm_direct import toy_task_2
 # from toy_task.GMM.algorithms.algorithm_stl import toy_task_3
 # from toy_task.GMM.algorithms.algorithm_p import toy_task_p
 from toy_task.GMM.utils.network_utils import set_seed
 
 
-@hydra.main(version_base=None, config_path="toy_task/GMM/conf", config_name="config_funnel")
+@hydra.main(version_base=None, config_path="toy_task/GMM/conf", config_name="config_gmm")
 def my_app(cfg: DictConfig) -> None:
     print(OmegaConf.to_yaml(cfg))
 
@@ -18,10 +18,10 @@ def my_app(cfg: DictConfig) -> None:
         **OmegaConf.to_container(cfg.schema, resolve=True)
     }
 
-    group_name = f"{cfg['exp_name']}_{cfg.target.model_name}_sweep_projection_parameters"
-    # run_name = f"seed_{cfg.seed.seed}"
-    run_name = f"seed_{cfg.seed.seed}_mean_{cfg.schema.eps_mean}_cov_{cfg.schema.eps_cov}_alpha_{cfg.schema.alpha}"
-    wandb.init(project="toy_task", group=group_name, config=config_dict, name=run_name)
+    group_name = f"{cfg['exp_name']}_{cfg.target.model_name}_adaption_from_1"
+    run_name = f"seed_{cfg.seed.seed}_batch_size_{cfg.target.batch_size}_gate_layers_{cfg.target.num_gate_layer}_gaussian_layers_{cfg.target.num_component_layer}_gate_lr_{cfg.target.gate_lr}_gaussian_lr_{cfg.target.gaussian_lr}"
+    # run_name = f"seed_{cfg.seed.seed}_mean_{cfg.schema.eps_mean}_cov_{cfg.schema.eps_cov}_alpha_{cfg.schema.alpha}"
+    wandb.init(project="spiral_gmm_target", group=group_name, config=config_dict, name=run_name)
 
     #1: decomposition; 2: direct; 3: stl; p: decom + gate projection
     toy_task(config_dict)
