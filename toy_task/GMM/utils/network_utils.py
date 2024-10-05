85d00f48f1dca64be6100db089f8d8b919cef46e
Luyao Deng <luyao.deng@student.kit.edu>
2024-10-05 16:55:56 +0200

    add autogradient method to calculate gradient given samples and fn

--- toy_task/GMM/utils/network_utils.py
+++ toy_task/GMM/utils/network_utils.py
@@ -1,3 +1,6 @@
+from typing import Callable, Tuple, Union
+
+
 import torch as ch
 import torch.nn as nn
 import numpy as np
@@ -72,3 +75,37 @@ def generate_init_biases(n_components, dim, scale):
     else:
         raise ValueError(f"Invalid dim {dim}. Now only support 2 or 10.")
     return init_bias_mean_list
+
+
+def eval_fn_grad(
+    fn: Callable, z: ch.Tensor, compute_grad: bool
+) -> Tuple[ch.Tensor, Union[ch.Tensor, None]]:
+    # check input
+    n_samples = z.shape[0]
+    batch_dim = z.shape[1:-1]
+    assert len(batch_dim) <= 1
+    d_z = z.shape[-1]
+
+    if not compute_grad:
+        f_z, f_z_grad, f_z_hess = _eval_fn(fn=fn, z=z), None, None
+    else:
+        z.requires_grad = True
+        f_z = _eval_fn(fn=fn, z=z)
+        f_z_grad = ch.zeros_like(z)
+        for i in range(n_samples):
+            if len(batch_dim) > 0:
+                for j in range(batch_dim[0]):
+                    f_z_grad[i, j] = ch.autograd.grad(
+                        outputs=f_z[i, j], inputs=z, retain_graph=True
+                    )[0][i, j]
+            else:
+                f_z_grad[i] = ch.autograd.grad(outputs=f_z[i], inputs=z, retain_graph=True)[0][i]
+        f_z_grad = f_z_grad.detach()
+
+    return f_z, f_z_grad
+
+
+def _eval_fn(fn, z: ch.Tensor) -> ch.Tensor:
+    # compute fn(z)
+    f_z = fn(z)
+    return f_z
