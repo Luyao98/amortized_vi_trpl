84fb31de205d15826308bdf866f75519e36c6156
Luyao Deng <luyao.deng@student.kit.edu>
2024-10-09 09:46:19 +0200

    cleanup

--- toy_task/GMM/targets/gmm_star_target.py
+++ toy_task/GMM/targets/gmm_star_target.py
@@ -1,82 +1,98 @@
-import math
+from typing import Callable
+
+
 import torch as ch
-from torch.distributions import uniform, MultivariateNormal, Categorical
-import numpy as np
-import matplotlib.pyplot as plt
-from toy_task.GMM.targets.abstract_target import AbstractTarget
+from torch.distributions import MultivariateNormal, Categorical, MixtureSameFamily
+import math
+
+from toy_task.GMM.targets.gaussian_mixture_target import ConditionalGMMTarget
 
 
-class ConditionalSTARTarget(AbstractTarget, ch.nn.Module):
-    def __init__(self, gate_fn, mean_fn, chol_fn, context_dim, context_bound_low=-3, context_bound_high=3):
-        super().__init__()
-        self.context_bound_low = context_bound_low
-        self.context_bound_high = context_bound_high
-        self.context_dim = context_dim
-        self.context_dist = uniform.Uniform(context_bound_low, context_bound_high)
-        self.gate_fn = gate_fn
-        self.mean_fn = mean_fn
-        self.chol_fn = chol_fn
+class ConditionalSTARTarget(ConditionalGMMTarget):
+    """
+    A conditional Gaussian Mixture Model target distribution.
+    """
+    def __init__(self,
+                 gate_fn: Callable,
+                 mean_fn: Callable,
+                 chol_fn: Callable,
+                 context_dim: int,
+                 context_bound_low: float = -3,
+                 context_bound_high: float = 3
+                 ):
+        """
+        Initializes the ConditionalSTARTarget with functions defining the GMM's components.
 
-    def get_contexts(self, n_contexts):
-        size = ch.Size([n_contexts, self.context_dim])
-        contexts = self.context_dist.sample(size)  # return shape(n_contexts, 1)
-        return contexts
+        Parameters:
+        - gate_fn (Callable): A function to generate the mixture weights given a context.
+        - mean_fn (Callable): A function to generate the means of the Gaussian components given a context.
+        - chol_fn (Callable): A function to generate the Cholesky decomposition of the covariance matrices given a context.
+        - context_dim (int): The dimensionality of the context.
+        - context_bound_low (float, optional): Lower bound for the uniform context distribution.
+        - context_bound_high (float, optional): Upper bound for the uniform context distribution.
+        """
+        super().__init__(gate_fn, mean_fn, chol_fn, context_dim, context_bound_low, context_bound_high)
 
-    def sample(self, contexts, n_samples):
+    def sample(self,
+               contexts: ch.Tensor,
+               n_samples: int
+               ) -> ch.Tensor:
+        """
+        Samples from the conditional GMM given contexts.
+
+        Parameters:
+        - contexts (ch.Tensor): The context vectors parameterizing the GMM with shape (n_contexts, context_dim).
+        - n_samples (int): The number of samples to draw.
+
+        Returns:
+        - ch.Tensor: shape (n_samples, n_contexts, d_z).
+        """
         device = contexts.device
-        gate = self.gate_fn(contexts).to(device)  # [n_contexts, n_components]
-        means = self.mean_fn(contexts).to(device) # [n_contexts, n_components, n_features]
-        chols = self.chol_fn(contexts).to(device) # [n_contexts, n_components, n_features, n_features]
+        log_gates = self.gate_fn(contexts).to(device)
+        means = self.mean_fn(contexts).to(device)
+        chols = self.chol_fn(contexts).to(device)
+        covs = chols @ chols.transpose(-1, -2)
 
-        indices = Categorical(ch.exp(gate)).sample(ch.Size([n_samples])).transpose(0, 1) # [n_contexts, n_samples]
-        chosen_means = ch.gather(means, 1, indices.unsqueeze(-1).expand(-1, -1, means.shape[-1]))
-        chosen_chols = ch.gather(chols, 1, indices.unsqueeze(-1).unsqueeze(-1).expand(-1, -1, means.shape[-1], means.shape[-1]))
-        chosen_covs = chosen_chols @ chosen_chols.transpose(-1, -2)
-        samples = MultivariateNormal(chosen_means, covariance_matrix=chosen_covs).sample() # [n_contexts, n_samples, n_features]
+        samples = MixtureSameFamily(
+            mixture_distribution=Categorical(logits=log_gates),
+            component_distribution=MultivariateNormal(loc=means, covariance_matrix=covs
+            ),
+        ).sample(ch.Size([n_samples,]))
         return samples
 
-    def log_prob_tgt(self, contexts, samples):
+    def log_prob_tgt(self,
+                     contexts: ch.Tensor,
+                     samples: ch.Tensor
+                     ) -> ch.Tensor:
+        """
+        Calculates the log-probability of samples under the conditional GMM.
+
+        Parameters:
+        - contexts (ch.Tensor): The context vectors parameterizing the GMM with shape (n_contexts, context_dim).
+        - samples (ch.Tensor):  shape (n_samples, n_contexts, d_z) or (n_samples, n_contexts, n_components, d_z)
+
+        Returns:
+        - ch.Tensor: Target log densities with shape (n_samples, n_contexts) or (n_samples, n_contexts, n_components).
+        """
         device = contexts.device
-        gate = self.gate_fn(contexts).to(device)  # [n_contexts, n_components]
-        means = self.mean_fn(contexts).to(device) # [n_contexts, n_components, n_features]
-        chols = self.chol_fn(contexts).to(device) # [n_contexts, n_components, n_features, n_features]
-        batch_size, n_components, n_features = means.shape
+        log_gates = self.gate_fn(contexts).to(device) # (n_contexts, n_components_tgt)
+        means = self.mean_fn(contexts).to(device) # (n_contexts, n_components_tgt, d_z)
+        chols = self.chol_fn(contexts).to(device) # (n_contexts, n_components_tgt, d_z, d_z)
+        covs = chols @ chols.transpose(-1, -2)  # (n_contexts, n_components_tgt, d_z, d_z)
 
+        tgt = MixtureSameFamily(
+            mixture_distribution=Categorical(logits=log_gates),
+            component_distribution=MultivariateNormal(loc=means, covariance_matrix=covs)
+        )
         if samples.dim() == 3:
-            n_samples = samples.shape[1]
+            log_probs = tgt.log_prob(samples)
         else:
-            # for plotting
-            n_samples = samples.shape[0]
-            samples = samples.unsqueeze(0).expand(batch_size, -1, -1)
+            n_samples, n_contexts, n_components, d_z = samples.shape
+            reshaped_samples = samples.permute(0, 2, 1, 3).reshape(-1, n_contexts, d_z)  # (n_samples * n_components, n_contexts, d_z)
+            reshaped_log_probs = tgt.log_prob(reshaped_samples)  # (n_samples * n_components, n_contexts)
+            log_probs = reshaped_log_probs.view(n_samples, n_components, n_contexts).permute(0, 2, 1)
 
-        gate_expanded = gate.unsqueeze(1).expand(-1, n_samples, -1)
-        means_expanded = means.unsqueeze(1).expand(-1, n_samples, -1, -1)
-        chols_expanded = chols.unsqueeze(1).expand(-1, n_samples, -1, -1, -1)
-        covs_expanded = chols_expanded @ chols_expanded.transpose(-1, -2)
-        samples_expanded = samples.unsqueeze(2).expand(-1, -1, n_components, -1)
-
-        mvn = MultivariateNormal(means_expanded, covariance_matrix=covs_expanded)
-        log_probs = mvn.log_prob(samples_expanded) + gate_expanded # [batch_size, n_samples, n_components]
-        log_probs = ch.logsumexp(log_probs, dim=2)  # [batch_size, n_samples]
         return log_probs
-
-    def visualize(self, contexts, n_samples=None):
-        fig, axes = plt.subplots(1, contexts.shape[0], figsize=(5 * contexts.shape[0], 5))
-        for i, c in enumerate(contexts):
-            x, y = np.meshgrid(np.linspace(-5, 5, 300), np.linspace(-5, 5, 300))
-            grid = ch.tensor(np.c_[x.ravel(), y.ravel()], dtype=ch.float32)
-            pdf_values = self.log_prob_tgt(c.unsqueeze(0), grid)
-            pdf_values = pdf_values.exp().view(300, 300).numpy()
-
-            ax = axes[i]
-            ax.contourf(x, y, pdf_values, levels=50, cmap='viridis')
-            if n_samples is not None:
-                samples = self.sample(c.unsqueeze(0), n_samples)
-                ax.scatter(samples[..., 0], samples[..., 1], color='red', alpha=0.5)
-            ax.set_title(f'Target {i + 1} with context {c}')
-
-        plt.tight_layout()
-        plt.show()
 
 
 def get_weights_fn(n_components):
@@ -97,16 +113,17 @@ def U(theta: float):
 
 def get_chol_fn(n_components):
     def cat_chol(c):
-        diag1 = ch.sin(c[:, 0]) + 1.1  # Shape: (batch_size,)
+        diag1 = ch.sin(c[:, 0]) + 1.1
         diag2 = 0.05 * ch.cos(c[:, 1]) + 0.08
         diag3 = 0.05 * ch.cos(c[:, 0]) + 0.08
+        diag4 = 0.05 * ch.sin(c[:, 0]) * ch.cos(c[:, 1])
         zeros = ch.zeros_like(c[:, 0])
         if c.shape[-1] == 1:
             chol = ch.stack([ch.stack([diag1, zeros], dim=1),
-                             ch.stack([zeros, diag3], dim=1)], dim=1)  # Shape: (batch_size, 2, 2)
+                             ch.stack([zeros, diag3], dim=1)], dim=1)
         elif c.shape[-1] == 2:
             chol = ch.stack([ch.stack([diag1, zeros], dim=1),
-                             ch.stack([zeros, diag2], dim=1)], dim=1)  # Shape: (batch_size, 2, 2)
+                             ch.stack([diag4, diag2], dim=1)], dim=1)
         else:
             raise ValueError("Context dimension must be 1 or 2")
         chols = [chol]
@@ -122,13 +139,13 @@ def get_mean_fn(n_components):
     def generate_star_means(contexts):
         batch_size = contexts.shape[0]
         # First component mean
-        mus = [ch.tensor([2.5, 0.0], device=contexts.device)]
+        mus = [ch.tensor([1.5, 0.0], device=contexts.device)]
         # Other components generated through rotation
         theta = 2 * math.pi / n_components
         rotation = U(theta).to(contexts.device)
         for _ in range(n_components - 1):
-            mus.append(rotation @ mus[-1])  # Rotate the previous mean
-        mu_true = ch.stack(mus, dim=0)  # Shape: (n_components, 2)
+            mus.append(rotation @ mus[-1])
+        mu_true = ch.stack(mus, dim=0)
         return mu_true.repeat(batch_size, 1, 1)
     return generate_star_means
 
@@ -137,18 +154,5 @@ def get_star_target(n_components, context_dim):
     gate_target = get_weights_fn(n_components)
     mean_target = get_mean_fn(n_components)
     chol_target = get_chol_fn(n_components)
-    gmm_target = ConditionalSTARTarget(gate_target, mean_target, chol_target, context_dim)
-    return gmm_target
-
-
-if __name__ == "__main__":
-
-    target = get_star_target(5, 2)
-    contexts = target.get_contexts(3)  # (3, 1)
-    # samples = target.sample(contexts, 1000)  # (3, 1000, 2)
-    target.visualize(contexts, n_samples=20)
-    # contexts = ch.tensor([[-0.3],
-    #                       [0.7],
-    #                       [-1.8]])
-    # print(ch.exp(target.gate_fn(contexts)))
-    # target.visualize(contexts)
+    star_target = ConditionalSTARTarget(gate_target, mean_target, chol_target, context_dim)
+    return star_target
--- toy_task/GMM/targets/test_target.py
+++ toy_task/GMM/targets/test_target.py
@@ -1,33 +1,37 @@
 import torch as ch
 
 
-from toy_task.GMM.targets.target_factory import get_gmm_target
-
+from toy_task.GMM.targets.target_factory import get_gmm_target, get_star_target
+from toy_task.GMM.utils.network_utils import set_seed
 
 if __name__ == "__main__":
 
-    # test gmm target
-    n_components = 10
-    context_dim = 2
-    n_contexts = 3
-    dz = 2
-    n_samples = 5
+    set_seed(1001)
 
-    target = get_gmm_target(n_components, context_dim)
-    test_ctx = target.get_contexts(n_contexts)  # (3, 2)
-    assert test_ctx.shape == (n_contexts, context_dim)
+    def test_gmm_target(n_components=10, context_dim=2, n_contexts=3, dz=2, n_samples=5):
+        target = get_gmm_target(n_components, context_dim)
+        test_ctx = target.get_contexts(n_contexts)  # (3, 2)
+        assert test_ctx.shape == (n_contexts, context_dim)
 
-    test_ctx = test_ctx.cuda()
-    test_samples = target.sample(test_ctx, n_samples)
-    assert test_samples.shape == (n_samples, n_contexts, dz)
+        test_ctx = test_ctx.cuda()
+        test_samples = target.sample(test_ctx, n_samples)
+        assert test_samples.shape == (n_samples, n_contexts, dz)
 
-    test_samples.requires_grad = True
-    test_log_prob = target.log_prob_tgt(test_ctx, test_samples)
-    assert test_log_prob.shape == (n_samples, n_contexts)
-    # target.visualize(test_ctx, n_samples=20)
-    # target.visualize(test_ctx)
+        test_samples.requires_grad = True
+        test_log_prob = target.log_prob_tgt(test_ctx, test_samples)
+        assert test_log_prob.shape == (n_samples, n_contexts)
+        # target.visualize(test_ctx, n_samples=20)
+        # target.visualize(test_ctx)
 
-    samples = ch.randn(n_samples, n_contexts, dz)
-    updated_samples = target.update_samples((test_ctx, samples), target.log_prob_tgt, 0.1, 100)
-    print(samples)
-    print(updated_samples)
+        samples = ch.randn(n_samples, n_contexts, dz)
+        updated_samples = target.update_samples((test_ctx, samples), target.log_prob_tgt, 0.1, 100)
+        print(samples)
+        print(updated_samples)
+
+
+    def test_star_target(n_components=7, context_dim=2, n_contexts=3, dz=2, n_samples=5):
+        target = get_star_target(n_components, context_dim)
+        contexts = target.get_contexts(n_contexts)
+        # samples = target.sample(contexts, n_samples)
+        # target.visualize(contexts, n_samples=20)
+        target.visualize(contexts)
