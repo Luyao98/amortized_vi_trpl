ca39879dce1dc0a3ab3d59146707991fe084fa6b
Luyao Deng <luyao.deng@student.kit.edu>
2024-11-01 10:27:24 +0100

    new config

---
+++ scripts/main_star_vanilla.py
@@ -0,0 +1,32 @@
+from omegaconf import DictConfig, OmegaConf
+import hydra
+import wandb
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from toy_task.GMM.algorithms.algorithm_vanilla import toy_task_vanilla
+from toy_task.GMM.utils.network_utils import set_seed
+
+
+@hydra.main(version_base=None, config_path="../toy_task/GMM/conf", config_name="config_star_vanilla")
+def star(cfg: DictConfig) -> None:
+    print(OmegaConf.to_yaml(cfg))
+
+    set_seed(cfg.star_target.seed)
+    config_dict = {
+        **OmegaConf.to_container(cfg.star_target, resolve=True)
+    }
+
+    project = "final2_star_target"
+    group_name = f"{cfg['exp_name']}"
+    run_name = f"seed_{cfg.star_target.seed}"
+    wandb.init(project=project, group=group_name, config=config_dict, name=run_name, dir="/home/temp_store/luyao")
+
+    toy_task_vanilla(config_dict)
+
+    wandb.finish()
+
+
+if __name__ == "__main__":
+    star()
